e39f655be8794347a2dd7f79e4f38bab923787dd
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-10-08 13:29:31 -0400

    set goal component

---
+++ components/ChallengeSetGoal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span
+      >I commit to read
+      <VInput id="set-goal" v-model="goal" label=""></VInput>
+      pages per day
+    </span>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api';
+import VInput from '@/components/VInput.vue';
+
+export default {
+  components: {
+    VInput,
+  },
+
+  setup() {
+    const goal = ref(0);
+
+    return {
+      goal,
+    };
+  },
+};
+</script>
---
+++ test/ChallengeSetGoal.spec.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils';
+import ChallengeSetGoal from '@/components/ChallengeSetGoal.vue';
+
+describe('ChallengeSetGoal', () => {
+  it('has input to set a goal', () => {
+    const wrapper = mount(ChallengeSetGoal);
+
+    const goalInput = wrapper.find('input#set-goal');
+
+    expect(goalInput.exists()).toBeTruthy();
+  });
+});
