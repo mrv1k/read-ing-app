e1d6634d51c040078adcf317a0baf41f97bd139a
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-10-10 07:42:59 -0400

    refactor custom input handler with watcher

--- components/ChallengeSetGoal.vue
+++ components/ChallengeSetGoal.vue
@@ -5,11 +5,10 @@
     </label>
     <input
       id="set-goal"
-      :value="goal"
+      v-model.number="goal"
       class="w-8 bg-gray-200 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500"
       type="text"
       placeholder="322"
-      @input="handleInput"
     />
     <label for="set-goal">
       pages per day
@@ -18,20 +17,15 @@
 </template>
 
 <script>
-import { ref } from '@vue/composition-api';
+import { ref, watch } from '@vue/composition-api';
 
 export default {
   setup(_, { root }) {
     const goal = ref(25);
-
-    const handleInput = (event) => {
-      goal.value = event.target.value;
-      root.$store.commit('challenge/SET_GOAL', goal.value);
-    };
+    watch(() => root.$store.commit('challenge/SET_GOAL', goal.value));
 
     return {
       goal,
-      handleInput,
     };
   },
 };
