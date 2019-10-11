e5be19011550b8343b49f90a784bbeb8b0081599
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-10-11 08:30:34 -0400

    make console.log and .error throw
    shamelessly stolen from Chris Fritz

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,6 +1,24 @@
+/* eslint-disable no-console */
 import VueTestUtils from '@vue/test-utils';
 import '@/plugins/composition-api';
 
 // Mock Nuxt components
 VueTestUtils.config.stubs['nuxt-link'] = '<a><slot /></a>';
 VueTestUtils.config.stubs['no-ssr'] = '<span><slot /></span>';
+
+/**
+ * Console handlers
+ */
+const error = console.error;
+console.error = function(message) {
+  error.apply(console, arguments);
+  // NOTE: You can whitelist some `console.error` messages here
+  // by returning if the `message` value is acceptable.
+  throw message instanceof Error ? message : new Error(message);
+};
+
+const warn = console.warn;
+console.warn = function(message) {
+  warn.apply(console, arguments);
+  throw message instanceof Error ? message : new Error(message);
+};
