d2a8d1184f70ea9152dd0b47e59ef54e29763d4c
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-10-11 10:12:01 -0400

    color background based on goal status

--- components/ProgressViewDay.vue
+++ components/ProgressViewDay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :class="goalStatus ? 'bg-green-500' : 'bg-red-500'">
     <div>
       {{ monthDay }}
     </div>
@@ -10,17 +10,23 @@
 </template>
 
 <script>
+import { computed } from '@vue/composition-api';
 import { format } from 'date-fns';
 
 export default {
-  setup() {
+  setup(_, { root }) {
     const today = new Date();
     const calendarDate = format(today, 'dd MMM yyyy');
     const monthDay = format(today, 'dd');
 
+    const goalStatus = computed(
+      () => root.$store.getters['challenge/goalStatus'],
+    );
+
     return {
       calendarDate,
       monthDay,
+      goalStatus,
     };
   },
 };
