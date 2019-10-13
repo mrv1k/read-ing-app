bd38df1195295edeb5496d046bc40fd38f2ae964
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-10-13 18:11:22 -0400

    test: cover book view table

--- test/components/BookViewTable.spec.js
+++ test/components/BookViewTable.spec.js
@@ -1,20 +1,23 @@
 import { mount } from '@vue/test-utils';
 import BookViewTable from '@/components/BookViewTable.vue';
+import { state as makeState } from '@/store/books';
+
+const requiredProps = {
+  headers: {
+    title: 'title',
+    author: 'author',
+    pages: 'pages',
+  },
+};
 
 describe('BookViewTable', () => {
   it('is a Vue instance', () => {
     const wrapper = mount(BookViewTable, {
-      propsData: {
-        headers: {
-          title: 'title',
-          author: 'author',
-          pages: 'pages',
-        },
-      },
+      propsData: { ...requiredProps },
       mocks: {
         $store: {
           state: {
-            books: {},
+            books: makeState(),
           },
         },
       },
