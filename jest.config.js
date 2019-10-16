00217ebb042df0f12c572e4ac29a3f78aaa80238
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-10-16 10:01:05 -0400

    chore: collect store and utils coverage

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
+    '<rootDir>/store/**/*.js',
+    '<rootDir>/utils/**/*.js',
   ],
 };
