fb209530460af7a5473a1bf0ebca47f9ea07b109
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-11-06 19:15:08 -0500

    add slug to month page

---
+++ pages/month/_month.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <MonthTable :month="month" />
+  </div>
+</template>
+
+<script>
+import { setMonth } from 'date-fns';
+import MonthTable from '@/components/MonthTable.vue';
+
+const now = new Date();
+
+export default {
+  components: {
+    MonthTable,
+  },
+  computed: {
+    month() {
+      return this.$route.params.month;
+    },
+    test() {
+      return this.month ? setMonth(now, this.month) : new Date().getMonth();
+    },
+  },
+};
+</script>
