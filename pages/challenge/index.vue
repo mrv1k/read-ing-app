b6b72135e6be427d74f3c0b61d5afecd09b2e981
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2019-11-07 08:50:31 -0500

    feat: add challenge defaults

---
+++ pages/challenge/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p>Q: Is this your first reading challenge?</p>
+    <span>A1: Default challenge is </span>
+    <span class="pb-4 font-semibold">
+      25 pages for 50 days starting right now
+    </span>
+    <p>
+      A2: or
+      <button
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+        @click.prevent="toggleCustomize"
+      >
+        Customize
+      </button>
+    </p>
+    <div v-if="customize">
+      <ChallengeSetGoal />
+      <br />
+      <ChallengeSetStart />
+      <br />
+      <ChallengeSetDuration />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api';
+import ChallengeSetGoal from '@/components/ChallengeSetGoal.vue';
+import ChallengeSetStart from '@/components/ChallengeSetStart.vue';
+import ChallengeSetDuration from '@/components/ChallengeSetDuration.vue';
+
+export default {
+  components: {
+    ChallengeSetGoal,
+    ChallengeSetStart,
+    ChallengeSetDuration,
+  },
+
+  setup() {
+    const customize = ref(false);
+    const toggleCustomize = () => (customize.value = !customize.value);
+
+    return { customize, toggleCustomize };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,5 @@
 <template>
   <div>
-    <div class="step-challenge">
-      <ChallengeSetGoal />
-      <br />
-      <ChallengeSetStart />
-      <br />
-      <ChallengeSetDuration />
-    </div>
+    <h1>Welcome, welcome. Come in. Grab a sit. Grab a book. Enjoy yourself!</h1>
   </div>
 </template>
-
-<script>
-import ChallengeSetGoal from '@/components/ChallengeSetGoal.vue';
-import ChallengeSetStart from '@/components/ChallengeSetStart.vue';
-import ChallengeSetDuration from '@/components/ChallengeSetDuration.vue';
-
-export default {
-  components: {
-    ChallengeSetGoal,
-    ChallengeSetStart,
-    ChallengeSetDuration,
-  },
-};
-</script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-</style>
